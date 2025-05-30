df70db903918771ebd15acccca300cea91ac4bab
Woody <devkritt@gmail.com>
2025-05-30 11:35:28 +0700

    create : init card on world space

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -4,4 +4,28 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
     public List<GameObject> cards;
+    public Transform deckParent;
+
+    [SerializeField] private float cardSpacing = 0.04f;
+
+    Vector3 pos;
+
+    private void Start()
+    {
+        pos = deckParent.position;
+
+        InitDeck();
+    }
+
+    private void InitDeck()
+    {
+        for (int i = 0; i < cards.Count; i++)
+        {
+            GameObject cardObj = Instantiate(cards[i], deckParent);
+            cardObj.transform.rotation = Quaternion.Euler(0, 0, 90);
+            cardObj.transform.position = pos + new Vector3(0, cardSpacing, 0);
+            pos = cardObj.transform.position;
+        }
+    }
+
 }
