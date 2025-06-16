4233b93e6fbd11a04c79f991aaa8a0956668db31
Woody <devkritt@gmail.com>
2025-06-16 22:03:28 +0700

    add : load deckSO from resource and set it to player and enemy

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
     public ClassSO classSO;
+    public DeckSO deckSO;
     public int HP = 20;
     public int ARMOR = 0;
 
-    public void SetClass(ClassSO _classSO)
+    public void SetData(ClassSO _classSO, DeckSO _deckSO)
     {
         classSO = _classSO;
+        deckSO = _deckSO;
     }
 }
--- Assets/Scripts/Match.cs
+++ Assets/Scripts/Match.cs
@@ -3,34 +3,41 @@ using UnityEngine;
 public class Match : MonoBehaviour
 {
     ClassSO[] classes;
+    DeckSO[] decks;
 
     ClassSO playerClass = null;
     ClassSO enemyClass = null;
+
+    DeckSO playerDeck = null;
+    DeckSO enemyDeck = null;
     
     public void Init() //Init would have to recieve player class, enemy class, player deck
     {
-        LoadMockupClassData();
+        LoadMockupDataFromResources();
 
         playerClass = classes[0]; // AAssuming the first class is for the player
         enemyClass = classes[1]; // Assuming the second class is for the enemy
 
-        InitPlayerData(playerClass);
-        InitEnemyData(enemyClass);
+        playerDeck = decks[1]; // Assuming the second deck is for the player
+        enemyDeck = decks[0]; // Assuming the first deck is for the enemy
+
+        InitPlayerData(playerClass, playerDeck);
+        InitEnemyData(enemyClass, enemyDeck);
     }
 
-    private void InitPlayerData(ClassSO _playerClass)
+    private void InitPlayerData(ClassSO _playerClass, DeckSO _playerDeck)
     {
         Player player = new GameObject("Player").AddComponent<Player>();
-        player.SetClass(_playerClass);
+        player.SetData(_playerClass, _playerDeck);
     }
 
-    private void InitEnemyData(ClassSO _enemyClass)
+    private void InitEnemyData(ClassSO _enemyClass, DeckSO _enemyDeck)
     {
         Enemy enemy = new GameObject("Enemy").AddComponent<Enemy>();
-        enemy.SetClass(_enemyClass);
+        enemy.SetData(_enemyClass, _enemyDeck);
     }
 
-    private void LoadMockupClassData()
+    private void LoadMockupDataFromResources()
     {
         classes = Resources.LoadAll<ClassSO>("Data/ClassSO");
         if (classes.Length == 0)
@@ -39,5 +46,11 @@ public class Match : MonoBehaviour
             return; 
         }
 
+        decks = Resources.LoadAll<DeckSO>("Data/DeckSO");
+        if (decks.Length == 0)
+        {
+            Debug.LogError("No DeckSO data found in Resources/Data/DeckSO.");
+            return; 
+        }
     }
 }
