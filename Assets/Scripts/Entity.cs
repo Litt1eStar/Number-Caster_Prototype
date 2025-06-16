c73d6fd58530a119976c0fcddc9ac5a0937809ae
Woody <devkritt@gmail.com>
2025-06-16 21:44:38 +0700

    add : set player and enemy class by using mockup data

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -5,4 +5,9 @@ public class Entity : MonoBehaviour
     public ClassSO classSO;
     public int HP = 20;
     public int ARMOR = 0;
+
+    public void SetClass(ClassSO _classSO)
+    {
+        classSO = _classSO;
+    }
 }
--- Assets/Scripts/Match.cs
+++ Assets/Scripts/Match.cs
@@ -4,27 +4,40 @@ public class Match : MonoBehaviour
 {
     ClassSO[] classes;
 
-    public void Init()
+    ClassSO playerClass = null;
+    ClassSO enemyClass = null;
+    
+    public void Init() //Init would have to recieve player class, enemy class, player deck
     {
         LoadMockupClassData();
-        InitPlayerData();
-        InitEnemyData();
+
+        playerClass = classes[0]; // AAssuming the first class is for the player
+        enemyClass = classes[1]; // Assuming the second class is for the enemy
+
+        InitPlayerData(playerClass);
+        InitEnemyData(enemyClass);
     }
 
-    private void InitPlayerData()
+    private void InitPlayerData(ClassSO _playerClass)
     {
         Player player = new GameObject("Player").AddComponent<Player>();
-        Debug.Log(player.name);
+        player.SetClass(_playerClass);
     }
 
-    private void InitEnemyData()
+    private void InitEnemyData(ClassSO _enemyClass)
     {
         Enemy enemy = new GameObject("Enemy").AddComponent<Enemy>();
-        Debug.Log(enemy.name);
+        enemy.SetClass(_enemyClass);
     }
 
     private void LoadMockupClassData()
     {
         classes = Resources.LoadAll<ClassSO>("Data/ClassSO");
+        if (classes.Length == 0)
+        {
+            Debug.LogError("No ClassSO data found in Resources/Data/ClassSO.");
+            return; 
+        }
+
     }
 }
