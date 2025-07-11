df79c4296acbdc4e4d0020621c47f8be24e57d3c
Woody <devkritt@gmail.com>
2025-07-12 04:37:24 +0700

    add : add logic to show stage detail  based on clicked stage

--- Assets/Scripts/UI/PVE_StageButton.cs
+++ Assets/Scripts/UI/PVE_StageButton.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public enum StageLevel
 {
@@ -6,10 +8,17 @@ public enum StageLevel
     MiniBoss,
     Boss,
 }
-public class PVE_StageButton : MonoBehaviour
+public class PVE_StageButton : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private ClassSO enemyClass;
     [SerializeField] private bool isClear = false;
     [SerializeField] private StageLevel stageLevel;
     [SerializeField] private Sprite stageSprite;
+    [SerializeField] private Transform t_stageDetail;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        t_stageDetail.gameObject.SetActive(true);
+        t_stageDetail.GetComponent<Image>().sprite = stageSprite;
+    }
 }
