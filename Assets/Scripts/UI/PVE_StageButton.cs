8985beead3ad651be5599c57b650d3e1c211b40f
Woody <devkritt@gmail.com>
2025-07-12 04:26:16 +0700

    add : create PVE_StageButton for assign data of each stage

---
+++ Assets/Scripts/UI/PVE_StageButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public enum StageLevel
+{
+    Monster,
+    MiniBoss,
+    Boss,
+}
+public class PVE_StageButton : MonoBehaviour
+{
+    [SerializeField] private ClassSO enemyClass;
+    [SerializeField] private bool isClear = false;
+    [SerializeField] private StageLevel stageLevel;
+}
