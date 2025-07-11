4a7a0cb2406e45fe4a215fc8cfd05d69a2cad905
Woody <devkritt@gmail.com>
2025-07-12 04:33:41 +0700

    edit : implement sprite and assign stageSprite to stage

--- Assets/Scripts/UI/PVE_StageButton.cs
+++ Assets/Scripts/UI/PVE_StageButton.cs
@@ -11,4 +11,5 @@ public class PVE_StageButton : MonoBehaviour
     [SerializeField] private ClassSO enemyClass;
     [SerializeField] private bool isClear = false;
     [SerializeField] private StageLevel stageLevel;
+    [SerializeField] private Sprite stageSprite;
 }
