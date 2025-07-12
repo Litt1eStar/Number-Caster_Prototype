50053bf65fcc198ca03ab87c5a010f309e27dcbd
Woody <devkritt@gmail.com>
2025-07-12 17:41:40 +0700

    add : add bgx and sfx for mainmenu and gameplay

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -9,20 +9,21 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private string deckSceneName = "Deck";
     public void NavigateToPlayMenu()
     {
+        AudioManager.Instance.PlaySFX("Button-Click");
         SceneManager.LoadScene(playMenuSceneName);
     }
 
     public void NavigateToProfile()
     {
-
+        AudioManager.Instance.PlaySFX("Button-Click");
     }
     public void NavigateToShop()
     {
-
+        AudioManager.Instance.PlaySFX("Button-Click");
     }
 
     public void NavigateToDeck()
     {
-
+        AudioManager.Instance.PlaySFX("Button-Click");
     }
 }
